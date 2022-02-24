895e700bdb798b5a7473d50315d99e13f4b81f69
Toni-Ty <Antonique2479@yahoo.com>
2022-02-24 13:45:08 -0600

    add initial packages and User controller, ran hello world get request, add and omit some dependencies

---
+++ ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/RamenNoodleTakeoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.ramennoodletakeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamenNoodleTakeoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamenNoodleTakeoutApplication.class, args);
+    }
+
+}
