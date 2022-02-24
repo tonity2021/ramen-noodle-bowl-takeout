4a9d7f1491f951c538a2494d97e86e5f56517ede
Toni-Ty <Antonique2479@yahoo.com>
2022-02-24 15:51:09 -0600

    User table complete with getters and setters

--- ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/model/User.java
+++ ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/model/User.java
@@ -6,6 +6,8 @@ public class User {
     private Long id;
     private String userName;
     private String password;
+
+
     private String emailAddress;
     private String phoneNumber;
     private String homeAddress;
@@ -19,4 +21,67 @@ public class User {
         this.phoneNumber = phoneNumber;
         this.homeAddress = homeAddress;
     }
+
+    //getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + userName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", homeAddress='" + homeAddress + '\'' +
+                '}';
+    }
+
 }
