91944b16d8c8a666af1a94421423fb72ceb16e2e
Toni-Ty <Antonique2479@yahoo.com>
2022-02-24 15:37:47 -0600

    add user constructor to User model

--- ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/model/User.java
+++ ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/model/User.java
@@ -3,4 +3,20 @@ package com.example.ramennoodletakeout.model;
 public class User {
 
 
+    private Long id;
+    private String userName;
+    private String password;
+    private String emailAddress;
+    private String phoneNumber;
+    private String homeAddress;
+
+
+    public User(Long id, String userName, String password, String emailAddress, String phoneNumber, String homeAddress) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.homeAddress = homeAddress;
+    }
 }
