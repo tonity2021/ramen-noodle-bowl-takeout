010d43d10d598b8618e68e93cecfa78c0e003781
Toni-Ty <Antonique2479@yahoo.com>
2022-02-24 18:07:37 -0600

    create user repository

--- ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/repository/UserRepository.java
+++ ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/repository/UserRepository.java
@@ -1,4 +1,17 @@
 package com.example.ramennoodletakeout.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserById(Long userId);
+    //to register
+    boolean existsByEmailAddress(String userEmailAddress);
+
+    //to login
+    User findUserByEmailAddress(String userEmailAddress);
+
 }
--- ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/service/OrderService.java
+++ ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/service/OrderService.java
@@ -1,4 +1,13 @@
 package com.example.ramennoodletakeout.service;
 
+import com.example.ramennoodletakeout.exceptions.InformationExistException;
+import com.example.ramennoodletakeout.model.Order;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class OrderService {
+
+
+
+
+
 }
