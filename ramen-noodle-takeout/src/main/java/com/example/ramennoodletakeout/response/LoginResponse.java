56c6e0e5fdaef36349d16003d61b2be73fc54119
Toni-Ty <Antonique2479@yahoo.com>
2022-02-24 21:34:00 -0600

    create LoginResponse class in response package

---
+++ ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/response/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.example.ramennoodletakeout.response;
+
+public class LoginResponse {
+
+    private String JWT;
+
+    public LoginResponse(String JWT) {
+        this.JWT = JWT;
+    }
+
+    public String getJWT() {
+        return JWT;
+    }
+
+    public void setJWT(String JWT) {
+        this.JWT = JWT;
+    }
+}
