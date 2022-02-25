2b07fe0c798b80454f87fa4c975090dcbca9413a
Toni-Ty <Antonique2479@yahoo.com>
2022-02-25 01:00:24 -0600

    create UserController

--- ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/controller/UserController.java
+++ ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/controller/UserController.java
@@ -1,15 +1,41 @@
 package com.example.ramennoodletakeout.controller;
 
+import com.example.ramennoodletakeout.request.LoginRequest;
+import com.example.ramennoodletakeout.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(path = "/api")
-
+@RequestMapping("/auth/users")
 public class UserController {
 
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    //register and log-in user
+    @PostMapping("/register/")
+    public User createUser(@RequestBody User userObject){
+        return userService.createUser(userObject);
+    }
+    @PostMapping("/login/")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){ return userService.loginUser(loginRequest);}
 
 }
-
-
