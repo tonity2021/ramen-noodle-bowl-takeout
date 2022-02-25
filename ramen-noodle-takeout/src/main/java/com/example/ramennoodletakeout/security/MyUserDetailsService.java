db7391a28b25cc289fe7e3a41f6249b466f94811
Toni-Ty <Antonique2479@yahoo.com>
2022-02-24 21:23:14 -0600

    create MyUserDetailsService

---
+++ ramen-noodle-takeout/src/main/java/com/example/ramennoodletakeout/security/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.ramennoodletakeout.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email){
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
+
+}
